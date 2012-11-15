b5247775b3ff4dbfbe5120a7a61db0741f278ce1
Lars Thorup <lars@zealake.com>
2012-11-14 16:03:08 -0800

    grunt test
    in progress

--- src/test/tests/util.test.js
+++ src/test/tests/util.test.js
@@ -1,12 +1,10 @@
 /*jshint undef:false, strict:false*/ // Note: to avoid having to write QUnit.module, etc
 require([
-    'models/card'
 ], function () {
     // Note: fails
     // var Card = require('models/card');
     module('util');
     test('simple', function () {
-        console.log('simple');
         equal(2 + 2, 4, 'kindergarten');
     });
 });
