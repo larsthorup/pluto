b81b479e959be78a8aa23cc04c059460de2f4674
Lars Thorup <lars@zealake.com>
2012-11-19 14:38:36 -0800

    cleanup

--- src/app/app.js
+++ src/app/app.js
@@ -14,7 +14,7 @@ define([
     'plugins/backbone.layoutmanager'
 ],
 
-function ($, _, Backbone, CardModel, CardView) {
+function ($, _, Backbone, Card, CardView) {
     'use strict';
 
     // Provide a global location to place configuration settings and module
@@ -25,58 +25,10 @@ function ($, _, Backbone, CardModel, CardView) {
     };
 
     // ToDo: load data from server
-    var model = new CardModel({title: 'Meet Rob'});
-    // ToDo: is this the right place to initialize the top view?
+    var model = new Card({title: 'Meet Rob'});
     app.view = new CardView({document: window.document, model: model});
     // ToDo: how to avoid having to render the view manually?
     app.view.render();
 
-    // Localize or create a new JavaScript Template object.
-    var JST = window.JST = window.JST || {};
-
-    // Configure LayoutManager with Backbone Boilerplate defaults.
-    Backbone.LayoutManager.configure({
-        // Allow LayoutManager to augment Backbone.View.prototype.
-        manage: true,
-
-        prefix: 'app/templates/',
-
-        fetch: function (path) {
-            // Concatenate the file extension.
-            path = path + '.html';
-
-            // If cached, use the compiled template.
-            if (JST[path]) {
-                return JST[path];
-            }
-
-            // Put fetch into `async-mode`.
-            var done = this.async();
-
-            // Seek out the template asynchronously.
-            $.get(app.root + path, function (contents) {
-                done(JST[path] = _.template(contents));
-            });
-        }
-    });
-
-    // Mix Backbone.Events, modules, and layout management into the app object.
-    return _.extend(app, {
-        // Create a custom object with a nested Views object.
-        module: function (additionalProps) {
-            return _.extend({ Views: {} }, additionalProps);
-        },
-
-        // Helper for using layouts.
-        useLayout: function (options) {
-            // Create a new Layout with options.
-            var layout = new Backbone.Layout(_.extend({
-                el: 'body'
-            }, options));
-
-            // Cache the refererence.
-            return this.layout = layout;
-        }
-    }, Backbone.Events);
-
+    return app;
 });
--- src/test/tests/views/card.test.js
+++ src/test/tests/views/card.test.js
@@ -2,11 +2,11 @@
 require([
     'views/card',
     'models/card'
-], function (CardView, CardModel) {
+], function (CardView, Card) {
     module('view.card', {
         setup: function () {
             var document = $('<div><div id="card"></div><script type="template/text" id="card-template"><span><%=title%></span></script></div>');
-            var model = new CardModel({title: 'Meet Rob'});
+            var model = new Card({title: 'Meet Rob'});
             this.cardView = new CardView({document: document, model: model});
             this.cardView.initialize();
         },
