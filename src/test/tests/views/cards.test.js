f3a31949a3c349f3ffbcf3d2e525fe9a14b2c0a1
Lars Thorup <lars@zealake.com>
2012-11-20 11:48:45 -0800

    backbone view composition
    in progress

---
+++ src/test/tests/views/cards.test.js
@@ -0,0 +1,9 @@
+/*jshint undef:false, strict:false*/ // Note: to avoid having to write QUnit.module, etc
+define(function (/*require*/) {
+
+    module('view/cards');
+
+    test('render', function () {
+        expect(0);
+    });
+});
