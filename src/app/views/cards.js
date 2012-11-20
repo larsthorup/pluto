d4540a763ca837a319011ee29133b0dd342b82cd
Lars Thorup <lars@zealake.com>
2012-11-20 14:05:59 -0800

    backbone collection view
    in progress

--- src/app/views/cards.js
+++ src/app/views/cards.js
@@ -1,7 +1,9 @@
 /*global define*/
 define(function (require) {
     'use strict';
+    var $ = require('jquery');
     var Backbone = require('backbone');
+    var _ = require('lodash');
 
     var CardsView = Backbone.View.extend({
 
@@ -12,11 +14,13 @@ define(function (require) {
         },
 
         initialize: function () {
-
+            this.document = this.options.document;
+            this.template = _.template($('#cards-template', this.document).html());
         },
 
         render: function () {
-
+            this.$el.html(this.template());
+            return this;
         }
     });
     return CardsView;
--- src/test/tests/views/cards.test.js
+++ src/test/tests/views/cards.test.js
@@ -1,10 +1,26 @@
 /*global define,QUnit*/
-define(function (/*require*/) {
+define(function (require) {
     'use strict';
+    var $ = require('jquery');
+    var CardsView = require('views/cards');
 
-    QUnit.module('view/cards');
+    QUnit.module('view/cards', {
+        setup: function () {
+            // given
+            var document = $('<div><div id="view"></div><script type="template/text" id="cards-template"><ul></ul></script></div>');
+            this.cardsView = new CardsView({
+                document: document,
+                el: $('#view', document)
+            });
+            this.cardsView.initialize();
+        }
+    });
 
     QUnit.test('render', function () {
-        QUnit.expect(0);
+        // when
+        this.cardsView.render();
+
+        // then
+        QUnit.equal(this.cardsView.$el.html(), '<ul></ul>', 'cardsView.html');
     });
 });
