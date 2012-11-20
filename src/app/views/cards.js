35398e1605a1bf96283b53c3a3f04379ff48f8a4
Lars Thorup <lars@zealake.com>
2012-11-20 11:48:58 -0800

    backbone view composition
    in progress

---
+++ src/app/views/cards.js
@@ -0,0 +1,23 @@
+/*global define*/
+define(function (require) {
+    'use strict';
+    var Backbone = require('backbone');
+
+    var CardsView = Backbone.View.extend({
+
+        events: function () {
+            return {
+
+            };
+        },
+
+        initialize: function () {
+
+        },
+
+        render: function () {
+
+        }
+    });
+    return CardsView;
+});
