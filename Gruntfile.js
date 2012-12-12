0239529a9ff28ba78f1a5b2f13ce6c932437ac38
Lars Thorup <lars@zealake.com>
2012-12-12 13:47:38 -0800

    grunt config refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,112 +3,124 @@ module.exports = function (grunt) {
     'use strict';
     var connect = require('connect');
 
-    // Project configuration.
-    grunt.initConfig({
-        clean: {
-            folder: 'output'
+
+    // convenience
+    grunt.registerTask('default', 'lint test');
+    grunt.registerTask('all', 'clean lint test coverage bundle');
+
+
+    // clean
+    grunt.loadNpmTasks('grunt-clean');
+    var cleanConfig = {
+        folder: 'output'
+    };
+
+
+    // lint
+    var lintConfig = {
+        all: [
+            'Gruntfile.js',
+            'src/app/**/*.js',
+            'src/test/*.js',
+            'src/test/tests/**/*.js'
+        ]
+    };
+    var jsHintOptions = {
+        options: { bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4,
+            latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true, quotmark: true,
+            regexp: true, undef: true, unused: true, strict: true, trailing: true
+        }
+    };
+
+
+    // test
+    var qunitConfig = {
+        src: ['src/test/index.html'],
+        bundle: ['output/bundle/test/index.html']
+    };
+    grunt.loadNpmTasks('grunt-junit');
+    grunt.registerTask('test', 'junit:env qunit:src');
+
+
+    // watch
+    var watchConfig = {
+        scripts: {
+            files: 'src/**/*.*',
+            tasks: 'lint test'
+        }
+    };
+
+
+    // coverage
+    grunt.loadNpmTasks('grunt-qunit-cov');
+    var coverageConfig = {
+        test:
+        {
+            minimum: 1.0,
+            baseDir: 'src',
+            srcDir: 'src/app',
+            depDirs: ['src/assets', 'src/test'],
+            outDir: 'output/coverage',
+            testFiles: ['src/test/index.html']
+        }
+    };
+    grunt.registerTask('coverage', 'qunit-cov');
+
+
+    // bundle
+    grunt.loadNpmTasks('grunt-requirejs');
+    var requirejsConfig = {
+        name: 'main',
+        dir: 'output/optimized',
+        appDir: 'src',
+        baseUrl: 'app',
+        paths: {
+            jquery: '../assets/js/libs/jquery',
+            underscore: '../assets/js/libs/lodash',
+            backbone: '../assets/js/libs/backbone'
         },
-        copy: {
-            bundle: {
-                files: {
-                    'output/bundle/index.html': 'output/optimized/index.html',
-                    'output/bundle/assets/css/index.css': 'output/optimized/assets/css/index.css',
-                    'output/bundle/assets/js/libs/require.js': 'output/optimized/assets/js/libs/require.js',
-                    'output/bundle/app/config.js': 'output/optimized/app/config.js',
-                    'output/bundle/app/main.js': 'output/optimized/app/main.js'
-                }
-            }
-        },
-        requirejs: {
-            name: 'main',
-            dir: 'output/optimized',
-            appDir: 'src',
-            baseUrl: 'app',
-            paths: {
-                jquery: '../assets/js/libs/jquery',
-                underscore: '../assets/js/libs/lodash',
-                backbone: '../assets/js/libs/backbone'
-            },
-            shim: {
-                backbone: {
-                    deps: ['underscore', 'jquery'],
-                    exports: 'Backbone'
-                }
-            }
-        },
-        'qunit-cov': {
-            test:
-            {
-                minimum: 1.0,
-                baseDir: 'src',
-                srcDir: 'src/app',
-                depDirs: ['src/assets', 'src/test'],
-                outDir: 'output/coverage',
-                testFiles: ['src/test/index.html']
-            }
-        },
-        watch: {
-            scripts: {
-                files: 'src/**/*.*',
-                tasks: 'lint coverage'
-            }
-        },
-        qunit: {
-            src: ['src/test/index.html'],
-            bundle: ['output/bundle/test/index.html']
-        },
-        lint: {
-            all: [
-                'Gruntfile.js',
-                'src/app/**/*.js',
-                'src/test/*.js',
-                'src/test/tests/**/*.js'
-            ]
-        },
-        jshint: {
-            options: {
-                bitwise: true,
-                camelcase: true,
-                curly: true,
-                eqeqeq: true,
-                forin: true,
-                immed: true,
-                indent: 4,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                noempty: true,
-                nonew: true,
-                plusplus: true,
-                quotmark: true,
-                regexp: true,
-                undef: true,
-                unused: true,
-                strict: true,
-                trailing: true
+        shim: {
+            backbone: {
+                deps: ['underscore', 'jquery'],
+                exports: 'Backbone'
             }
         }
-    });
-
-    grunt.loadNpmTasks('grunt-junit');
-    grunt.loadNpmTasks('grunt-qunit-cov');
-    grunt.loadNpmTasks('grunt-requirejs');
-    grunt.loadNpmTasks('grunt-clean');
+    };
     grunt.loadNpmTasks('grunt-contrib-copy');
-
-    // default and alias tasks
-    grunt.registerTask('test:bundle', 'qunit:bundle');
+    var copyConfig = {
+        bundle: {
+            files: {
+                'output/bundle/index.html': 'output/optimized/index.html',
+                'output/bundle/assets/css/index.css': 'output/optimized/assets/css/index.css',
+                'output/bundle/assets/js/libs/require.js': 'output/optimized/assets/js/libs/require.js',
+                'output/bundle/app/config.js': 'output/optimized/app/config.js',
+                'output/bundle/app/main.js': 'output/optimized/app/main.js'
+            }
+        }
+    };
     grunt.registerTask('bundle', 'requirejs copy:bundle');
-    grunt.registerTask('coverage', 'qunit-cov');
-    grunt.registerTask('test', 'junit:env qunit:src');
-    grunt.registerTask('default', 'lint test');
-    grunt.registerTask('serve:src', 'HTTP serve src on port 8080', function () {
+    // ToDo: grunt.registerTask('test:bundle', 'qunit:bundle');
+
+
+    // serve
+    var serve = function (path, port) {
         // Note: using connect['static'], as jshint complains about connect.static, because static is a reserved word
-        connect(connect['static']('src')).listen(8080);
+        connect(connect['static'](path)).listen(port);
+    };
+    grunt.registerTask('serve:src', 'HTTP serve src on port 8080', function () { serve('src', 8080); });
+    grunt.registerTask('serve:bundle', 'HTTP serve bundle on port 8081', function () { serve('output/bundle', 8081); });
+
+
+    // grunt
+    grunt.initConfig({
+        clean: cleanConfig,
+        lint: lintConfig,
+        jshint: jsHintOptions,
+        qunit: qunitConfig,
+        watch: watchConfig,
+        'qunit-cov': coverageConfig,
+        requirejs: requirejsConfig,
+        copy: copyConfig
     });
-    grunt.registerTask('serve:bundle', 'HTTP serve bundle on port 8081', function () {
-        // Note: using connect['static'], as jshint complains about connect.static, because static is a reserved word
-        connect(connect['static']('output/bundle')).listen(8081);
-    });
-    grunt.registerTask('all', 'clean lint test coverage bundle');
+
 };
