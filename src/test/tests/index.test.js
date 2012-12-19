2cdadb05f16a5103610ef4b379e0cd65f77f7920
Lars Thorup <lars@zealake.com>
2012-12-19 14:45:11 -0800

    integration test
    now mocking server response

--- src/test/tests/index.test.js
+++ src/test/tests/index.test.js
@@ -1,4 +1,4 @@
-/*global define,QUnit*/
+/*global define,QUnit,window*/
 define(function (require) {
     'use strict';
     var q$ = require('jquery');
@@ -13,7 +13,9 @@ define(function (require) {
         }
     });
 
+    // ToDo: hide the plumbing
     QUnit.asyncTest('login-then-view', function () {
+        // when we ask for the login page
         q$('#qunit-fixture').html('<iframe id="appUnderTest" src="/#login"></iframe>');
         var $appUnderTest = q$('#appUnderTest');
         $appUnderTest.load(function () {
@@ -21,8 +23,45 @@ define(function (require) {
             windowUnderTest.require.onResourceLoad = function (context, map/*, depArray*/) {
                 if (map.url === 'app/main.js') { // Note: now all application modules have been loaded
                     var $ = windowUnderTest.require('jquery');
-                    QUnit.equal($('#header h1').text(), 'Pluto', '#header');
-                    QUnit.start();
+                    $.getScript('/test/libs/jquery.mockjax.js', function () {
+
+                        // then we see the login page
+                        QUnit.equal($('#header h1').text(), 'Pluto', '#header');
+                        QUnit.equal($.trim($('#main label.userLabel').text()), 'User Token:', '.userLabel');
+
+                        // given mocked server response
+                        $.mockjax({
+                            log: null,
+                            url: 'https://api.trello.com/1/lists/509070d37b1e65530d005067',
+                            data: {
+                                key: '4c5b4d16e6e53d893674f9452ac277bf', // ToDo: this.trello.appKey,
+                                token: 'lars',
+                                cards: 'open'
+                            },
+                            responseTime: 1,
+                            responseText: {
+                                cards: [{id: 42, name: 'play!'}]
+                            }
+                        });
+
+
+                        // when we enter a user token and click the login button
+                        var secretUserToken = 'lars';
+                        $('#main input.user').val(secretUserToken);
+                        $('#main a.login').trigger('click');
+
+                        // ToDo figure out how to wait for page to change
+                        window.setTimeout(function () {
+
+                            // then we see the list of cards
+                            var $cards = $('#main ul.items li');
+                            QUnit.equal($cards.length, 1, 'li.length');
+                            QUnit.equal($cards.first().text(), 'play!', 'li.text');
+
+                            QUnit.start();
+                        }, 2000);
+
+                    });
                 }
             };
         });
