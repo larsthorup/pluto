04029b09122fe54d08636b76a1921d50d93e0e0a
Lars Thorup <lars@zealake.com>
2012-12-19 13:24:00 -0800

    integration test
    now starts running as soon as application has finished loading

--- src/test/tests/index.test.js
+++ src/test/tests/index.test.js
@@ -1,4 +1,4 @@
-/*global define,QUnit,window*/
+/*global define,QUnit*/
 define(function (require) {
     'use strict';
     var q$ = require('jquery');
@@ -15,13 +15,16 @@ define(function (require) {
 
     QUnit.asyncTest('login-then-view', function () {
         q$('#qunit-fixture').html('<iframe id="appUnderTest" src="/#login"></iframe>');
-        q$('#appUnderTest').load(function () {
-            // ToDo: how to wait until jquery (and everything else) have been loaded?
-            window.setTimeout(function () {
-                var $ = window.document.getElementById('appUnderTest').contentWindow.require('jquery');
-                QUnit.equal($('#header h1').text(), 'Pluto', '#header');
-                QUnit.start();
-            }, 1000);
+        var $appUnderTest = q$('#appUnderTest');
+        $appUnderTest.load(function () {
+            var windowUnderTest = $appUnderTest[0].contentWindow;
+            windowUnderTest.require.onResourceLoad = function (context, map/*, depArray*/) {
+                if (map.url === 'app/main.js') { // Note: now all application modules have been loaded
+                    var $ = windowUnderTest.require('jquery');
+                    QUnit.equal($('#header h1').text(), 'Pluto', '#header');
+                    QUnit.start();
+                }
+            };
         });
     });
 });
