4d66b5f39a79896d38db56d2137279f9e30db89e
Lars Thorup <lars@zealake.com>
2012-12-19 20:39:01 -0800

    refactoring
    IQUnit introduced

--- src/test/tests/index.test.js
+++ src/test/tests/index.test.js
@@ -13,81 +13,92 @@ define(function (require) {
         }
     });
 
-    // ToDo: hide the plumbing
-    QUnit.asyncTest('login-then-view', function () {
-        // when we ask for the login page
-        q$('#qunit-fixture').html('<iframe id="appUnderTest" src="/#login"></iframe>');
-        var $appUnderTest = q$('#appUnderTest');
-        $appUnderTest.load(function () {
-            var windowUnderTest = $appUnderTest[0].contentWindow;
-            var require = windowUnderTest.require;
-            require.onResourceLoad = function (context, map/*, depArray*/) {
-                if (map.url === 'app/main.js') { // Note: now all application modules have been loaded
-                    var $ = require('jquery');
-                    $.getScript('/test/libs/jquery.mockjax.js', function () {
-
-                        var waitFor = function (selector, timeout) {
-                            var waitForDeferred = function (selector, dfd, timeout) {
-                                var result = $(selector);
-                                if (result.length > 0) {
-                                    dfd.resolve(result);
-                                } else {
-                                    var waitTime = 50;
-                                    if (timeout < waitTime) {
-                                        dfd.reject();
-                                    } else {
-                                        window.setTimeout(function () {
-                                            waitForDeferred(selector, dfd, timeout - waitTime);
-                                        }, waitTime);
-                                    }
-                                }
-                            };
-                            var dfd = $.Deferred();
-                            waitForDeferred(selector, dfd, timeout);
-                            return dfd.promise();
-                        };
-
-                        // given
-                        var Trello = require('persistence/trello');
-                        var trello = new Trello();
-
-                        // then we see the login page
-                        QUnit.equal($('#header h1').text(), 'Pluto', '#header');
-                        QUnit.equal($.trim($('#main label.userLabel').text()), 'User Token:', '.userLabel');
-
-                        // given mocked server response
-                        var listId = '509070d37b1e65530d005067';
-                        var mockUrl = trello.url + '/lists/' + listId;
-                        $.mockjax({
-                            log: null,
-                            url: mockUrl,
-                            data: {
-                                key: trello.appKey,
-                                token: 'lars',
-                                cards: 'open'
-                            },
-                            responseTime: 1,
-                            responseText: {
-                                cards: [{id: 42, name: 'play!'}]
-                            }
-                        });
-
-
-                        // when we enter a user token and click the login button
-                        $('#main input.user').val('lars');
-                        $('#main a.login').trigger('click');
-
-                        // then we see the list of cards
-                        var promise = waitFor('#main ul.items li', 100);
-                        promise.done(function ($cards) {
-                            QUnit.equal($cards.length, 1, 'li.length');
-                            QUnit.equal($cards.first().text(), 'play!', 'li.text');
-
-                            QUnit.start();
-                        });
-                    });
+    // Note: Interactive QUnit
+    var IQUnit = {
+        // ToDo: create waitFor as a jQuery plugin
+        // http://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+        waitFor: function (jQuery, selector, timeout) {
+            var waitForDeferred = function (jQuery, selector, dfd, timeout) {
+                var result = jQuery(selector);
+                if (result.length > 0) {
+                    dfd.resolve(result);
+                } else {
+                    var waitTime = 50;
+                    if (timeout < waitTime) {
+                        dfd.reject();
+                    } else {
+                        window.setTimeout(function () {
+                            waitForDeferred(jQuery, selector, dfd, timeout - waitTime);
+                        }, waitTime);
+                    }
                 }
             };
+            var dfd = jQuery.Deferred();
+            waitForDeferred(jQuery, selector, dfd, timeout);
+            return dfd.promise();
+        },
+
+        asyncTest: function (testName/*, expected*/, pageUrl, callback) {
+            return QUnit.asyncTest(testName/*, expected*/, function () {
+                // Note: first we load the referenced page into an iframe in QUnit's HTML fixture so we can interact with it
+                q$('#qunit-fixture').html('<iframe id="appUnderTest" src="' + pageUrl + '"></iframe>');
+                var $appUnderTest = q$('#appUnderTest');
+                $appUnderTest.load(function () {
+                    // Note: we can get a reference to the local page's require object as soon as the iframe is loaded
+                    var windowUnderTest = $appUnderTest[0].contentWindow;
+                    var require = windowUnderTest.require;
+                    // Note: we know that all dependent modules have been loaded when the outermost dependency, app/main.js, has been loaded
+                    require.onResourceLoad = function (context, map/*, depArray*/) {
+                        if (map.url === 'app/main.js') {
+                            // Note: so then we can get a reference to the jQuery object on the local page
+                            var $ = require('jquery');
+                            // Note: using the jQuery object we can load mockjax into the local page
+                            $.getScript('/test/libs/jquery.mockjax.js', function () {
+                                callback(require, $);
+                            });
+                        }
+                    };
+                });
+            });
+        }
+    };
+
+    // ToDo: create an AppDriver to make this test more readable
+    IQUnit.asyncTest('login-then-view', '/#login', function (require, $) {
+        // then first we see the login page
+        QUnit.equal($('#header h1').text(), 'Pluto', '#header');
+        QUnit.equal($.trim($('#main label.userLabel').text()), 'User Token:', '.userLabel');
+
+        // given mocked server response
+        var Trello = require('persistence/trello');
+        var trello = new Trello();
+        var listId = '509070d37b1e65530d005067';
+        var mockUrl = trello.url + '/lists/' + listId;
+        $.mockjax({
+            log: null,
+            url: mockUrl,
+            data: {
+                key: trello.appKey,
+                token: 'lars',
+                cards: 'open'
+            },
+            responseTime: 1,
+            responseText: {
+                cards: [{id: 42, name: 'play!'}]
+            }
+        });
+
+        // when we enter a user token and click the login button
+        $('#main input.user').val('lars');
+        $('#main a.login').trigger('click');
+
+        // then we see the list of cards
+        var promise = IQUnit.waitFor($, '#main ul.items li', 100);
+        promise.done(function ($cards) {
+            QUnit.equal($cards.length, 1, 'li.length');
+            QUnit.equal($cards.first().text(), 'play!', 'li.text');
+
+            QUnit.start();
         });
     });
 });
