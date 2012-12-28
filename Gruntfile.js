418789e67813faa8ae9a7de127b6952585bb4491
Lars Thorup <lars@zealake.com>
2012-12-28 15:16:29 -0800

    investigating teamcity lint error display

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*global module*/
-/*jshint camelcase:false*/ // because of gruntConfig.qunit_junit
+/*jshint camelcase:true*/ // because of gruntConfig.qunit_junit
 module.exports = function (grunt) {
     'use strict';
 
